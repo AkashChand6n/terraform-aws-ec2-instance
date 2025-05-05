81a86bb96d1d5bbcbf65fc99fab0029f36c46b64
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-05-05 22:40:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,4 +6,16 @@ variable "ami" {
 variable "instance_type" {
   description = "The instance type for the EC2 instance"
   type        = string
+  default     = "t2.micro"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = "The name of the AWS key pair to use for SSH access"
+  type        = string
 }
