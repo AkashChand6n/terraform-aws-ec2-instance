1a875e0f535d5af8da863d11507df78eecf3e0bf
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-05-05 22:40:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_instance" "My-test-ec2" {
-  ami = var.ami
+  ami           = var.ami
   instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "My-Test-EC2"
+  }
 }
