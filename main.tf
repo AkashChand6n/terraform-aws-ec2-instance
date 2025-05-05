39159cf0df04488feb3c8aba6d36e9131af0a5dc
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-05-05 14:38:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
 terraform {
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
+
 provider "aws" {
   region = "us-east-1"
-  
 }
+
 resource "aws_instance" "registry-instance" {
-  ami = var.my-ami
-  instance_type = var.my-instance-type
+  ami           = var.my_ami
+  instance_type = var.my_instance_type
+
   tags = {
     Name = "registry-instance"
   }
