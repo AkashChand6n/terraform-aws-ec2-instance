b855f69525ce7ab57ee4cab0f64589e85d1be846
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-05-05 14:41:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "registry-instance" {
-  ami           = var.my_ami
-  instance_type = var.my_instance_type
-
-  tags = {
-    Name = "registry-instance"
-  }
+resource "aws_instance" "My-test-ec2" {
+  ami = var.ami
+  instance_type = var.instance_type
 }
